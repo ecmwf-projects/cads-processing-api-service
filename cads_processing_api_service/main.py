dae2faf1f0b0d4d1561c54931f7189ada8c891b9
Marco Cucchi <m.cucchi@bopen.eu>
2022-06-24 14:56:17 +0200

    dummy client

--- cads_processing_api_service/main.py
+++ cads_processing_api_service/main.py
@@ -1,3 +1,39 @@
+import urllib.parse
+from typing import Any
+
+import attrs
+import fastapi
 from ogc_api_processes_fastapi import main
 
-app = main.instantiate_app()
+PROCESS_LIST: list[dict[str, str]] = [
+    {"id": "retrieve-reanalysis-era5-single-levels", "version": "0.1"},
+    {"id": "retrieve-reanalysis-era5-pressure-levels", "version": "0.1"},
+    {"id": "retrieve-reanalysis-era5-land", "version": "0.1"},
+    {"id": "retrieve-reanalysis-era5-land-monthly-means", "version": "0.2"},
+]
+
+
+@attrs.define
+class DummyClient(main.BaseClient):  # type: ignore
+    """
+    Dummy implementation of the OGC API - Processes endpoints.
+    """
+
+    def get_processes(
+        self, request: fastapi.Request
+    ) -> dict[str, list[dict[str, Any]]]:
+
+        links = [
+            {
+                "href": urllib.parse.urljoin(str(request.base_url), "processes"),
+                "rel": "self",
+            }
+        ]
+        processes_list = PROCESS_LIST
+
+        retval = {"processes": processes_list, "links": links}
+
+        return retval
+
+
+app = main.instantiate_app(client=DummyClient())
