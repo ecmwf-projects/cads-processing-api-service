d2edb34b579b0beac858277a161353bcc1dd4cd2
barghini <a.barghini@bopen.eu>
2022-12-06 17:01:06 +0100

    fix tests

--- tests/test_20_adaptors.py
+++ tests/test_20_adaptors.py
@@ -25,10 +25,10 @@ def test_make_system_job_kwargs_default() -> None:
 
     exp_setup_code = adaptors.FALLBACK_SETUP_CODE
     config = adaptors.FALLBACK_CONFIG.copy()
-    exp_entry_point = config.pop("entry_point")
+    exp_entry_point = adaptors.FALLBACK_ENTRY_POINT
     exp_kwargs = {
         "request": inputs,
-        "config": config | {"collection_id": process_id} | {"mapping": {}},
+        "config": config | {"collection_id": process_id},
     }
 
     job_kwargs = adaptors.make_system_job_kwargs(
@@ -37,4 +37,5 @@ def test_make_system_job_kwargs_default() -> None:
 
     assert job_kwargs["setup_code"] == exp_setup_code
     assert job_kwargs["entry_point"] == exp_entry_point
+
     assert job_kwargs["kwargs"] == exp_kwargs
