c391131bf47bf560966be814abf136c4b42b0db9
Marco Cucchi <m.cucchi@bopen.eu>
2023-02-06 10:50:03 +0100

    set pool_recylce from env

--- cads_processing_api_service/dependencies.py
+++ cads_processing_api_service/dependencies.py
@@ -15,6 +15,7 @@
 # limitations under the License
 
 import functools
+import os
 from typing import Iterator
 
 import cads_broker.config
@@ -31,6 +32,7 @@ def get_compute_session_maker() -> sqlalchemy.orm.sessionmaker:
     broker_engine = sqlalchemy.create_engine(
         broker_settings.connection_string,
         pool_timeout=0.1,
+        pool_recycle=int(os.environ.get("POOL_RECYCLE")),  # type: ignore
     )
     return sqlalchemy.orm.sessionmaker(broker_engine)
 
@@ -47,7 +49,10 @@ def get_compute_session() -> Iterator[sqlalchemy.orm.Session]:
 @functools.lru_cache()
 def get_catalogue_session_maker() -> sqlalchemy.orm.sessionmaker:
     catalogue_settings = cads_catalogue.config.ensure_settings()
-    catalogue_engine = sqlalchemy.create_engine(catalogue_settings.connection_string)
+    catalogue_engine = sqlalchemy.create_engine(
+        catalogue_settings.connection_string,
+        pool_recycle=int(os.environ.get("POOL_RECYCLE")),  # type: ignore
+    )
     return sqlalchemy.orm.sessionmaker(catalogue_engine)
 
 
