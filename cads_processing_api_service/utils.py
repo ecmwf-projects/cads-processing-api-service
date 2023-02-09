15d98731b085535dcaf28b711bc14681d58424a3
Alessandro Amici <a.amici@bopen.eu>
2023-02-09 19:16:16 +0100

    Try use joinedload

--- cads_processing_api_service/utils.py
+++ cads_processing_api_service/utils.py
@@ -64,7 +64,6 @@ def lookup_resource_by_id(
     except sqlalchemy.orm.exc.NoResultFound as exc:
         logger.exception(repr(exc))
         raise ogc_api_processes_fastapi.exceptions.NoSuchProcess()
-    row.licences
     session.expunge(row)
     return row
 
