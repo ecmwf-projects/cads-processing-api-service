b4e0c4673715cc3fd8367eac01cc478ced0105aa
Alessandro Amici <a.amici@bopen.eu>
2023-02-09 19:07:29 +0100

    Typo

--- cads_processing_api_service/utils.py
+++ cads_processing_api_service/utils.py
@@ -64,7 +64,7 @@ def lookup_resource_by_id(
     except sqlalchemy.orm.exc.NoResultFound as exc:
         logger.exception(repr(exc))
         raise ogc_api_processes_fastapi.exceptions.NoSuchProcess()
-    row.liceces
+    row.licences
     session.expunge(row)
     return row
 
