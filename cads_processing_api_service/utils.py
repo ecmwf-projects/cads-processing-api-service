6bb5707567481de4aff191523036c7dc7b1a25c1
Alessandro Amici <a.amici@bopen.eu>
2023-02-09 19:06:01 +0100

    Try load

--- cads_processing_api_service/utils.py
+++ cads_processing_api_service/utils.py
@@ -64,6 +64,7 @@ def lookup_resource_by_id(
     except sqlalchemy.orm.exc.NoResultFound as exc:
         logger.exception(repr(exc))
         raise ogc_api_processes_fastapi.exceptions.NoSuchProcess()
+    row.liceces
     session.expunge(row)
     return row
 
