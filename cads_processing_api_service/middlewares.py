658dbf5f118bfd582647b5032d427e4d27195f5c
Marco Cucchi <m.cucchi@bopen.eu>
2023-02-09 17:02:09 +0100

    remove logging middleware

--- cads_processing_api_service/middlewares.py
+++ cads_processing_api_service/middlewares.py
@@ -14,44 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import uuid
-
 import fastapi
 import starlette
-import structlog
 
 from . import config
-
-
-# See https://github.com/snok/asgi-correlation-id/blob/5a7be6337f3b33b84a00d03baae3da999bb722d5/asgi_correlation_id/middleware.py  # noqa: E501
-class LoggerInitializationMiddleware:
-    def __init__(self, app: starlette.types.ASGIApp):
-        self.app = app
-
-    async def __call__(
-        self,
-        scope: starlette.types.Scope,
-        receive: starlette.types.Receive,
-        send: starlette.types.Send,
-    ):
-        if scope["type"] != "http":
-            return await self.app(scope, receive, send)
-
-        # request = fastapi.Request(scope, receive=receive)
-        # trace_id = request.headers.get("X-Trace-ID", None)
-
-        async def send_with_trace_id(message):
-            if message["type"] == "http.response.start":
-                headers = starlette.datastructures.MutableHeaders(scope=message)
-                headers.append("X-Trace-Id", trace_id)
-
-            await send(message)
-
-        structlog.contextvars.clear_contextvars()
-        trace_id = str(uuid.uuid4())
-        structlog.contextvars.bind_contextvars(trace_id=trace_id)
-        await self.app(scope, receive, send_with_trace_id)
-
 
 CACHEABLE_HTTP_METHODS = ["GET", "HEAD"]
 
