501f3fc99f625c16828f7762675def77faba90d4
Marco Cucchi <m.cucchi@bopen.eu>
2023-03-08 16:47:42 +0100

    add processDescription field to StatusInfo

--- cads_processing_api_service/models.py
+++ cads_processing_api_service/models.py
@@ -36,6 +36,7 @@ class StatusInfo(ogc_api_processes_fastapi.models.StatusInfo):
         | list[ogc_api_processes_fastapi.models.InlineOrRefData],
     ]
     results: dict[str, Any] | None = None
+    processDescription: dict[str, Any] | None = None
 
 
 class JobList(ogc_api_processes_fastapi.models.JobList):
