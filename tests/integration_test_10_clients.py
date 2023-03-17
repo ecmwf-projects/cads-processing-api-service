7c22e23084f56a12295972c32b4ddce7bbfc3f08
Marco Cucchi <m.cucchi@bopen.eu>
2023-03-17 14:46:55 +0100

    fix int test

--- tests/integration_test_10_clients.py
+++ tests/integration_test_10_clients.py
@@ -135,7 +135,7 @@ def test_get_processes(dev_env_proc_api_url: str) -> None:
     assert all([key in response_body for key in exp_keys])
 
     number_of_processes = len(response_body["processes"])
-    exp_number_of_processes = 7
+    exp_number_of_processes = 10
     assert number_of_processes == exp_number_of_processes
 
 
