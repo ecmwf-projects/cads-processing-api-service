a8e61e1718aa3232a7b79f78b2459cdef15fdcb9
Marco Cucchi <m.cucchi@bopen.eu>
2023-03-31 16:09:49 +0200

    change grouped_path

--- cads_processing_api_service/middlewares.py
+++ cads_processing_api_service/middlewares.py
@@ -52,6 +52,6 @@ class ProcessingPrometheusMiddleware(starlette_exporter.PrometheusMiddleware):
     def _get_router_path(scope: starlette.types.Scope) -> str | None:
         path = scope.get("path", "")
         if path.startswith("/jobs/") and path != "/jobs/":
-            return "/jobs/{job_id}"
+            return "/jobs/(job_id)"
         else:
             return None
