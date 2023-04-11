700a855c9c17d92875a55ae60ad95ccafc66a730
Marco Cucchi <m.cucchi@bopen.eu>
2023-04-11 16:56:26 +0200

    make apply_constraints async

--- cads_processing_api_service/constraints.py
+++ cads_processing_api_service/constraints.py
@@ -9,14 +9,16 @@ import fastapi
 from . import adaptors, db_utils, exceptions, utils
 
 
-def apply_constraints(
+async def apply_constraints(
     process_id: str = fastapi.Path(...),
     request: dict[str, Any] = fastapi.Body(...),
 ) -> dict[str, list[str]]:
     record = cads_catalogue.database.Resource
-    catalogue_sessionmaker = db_utils.get_catalogue_sessionmaker()
-    with catalogue_sessionmaker() as catalogue_session:
-        dataset = utils.lookup_resource_by_id(process_id, record, catalogue_session)
+    catalogue_sessionmaker = db_utils.get_catalogue_async_sessionmaker()
+    async with catalogue_sessionmaker() as catalogue_session:
+        dataset = await utils.lookup_resource_by_id(
+            process_id, record, catalogue_session
+        )
     adaptor: cads_adaptors.adaptor.AbstractAdaptor = adaptors.instantiate_adaptor(
         dataset
     )
