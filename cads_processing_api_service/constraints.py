6e72558896047ffd1f1bb9f3c730ac602a7d50d6
Marco Cucchi <m.cucchi@bopen.eu>
2023-07-12 12:34:32 +0200

    explicit import

--- cads_processing_api_service/constraints.py
+++ cads_processing_api_service/constraints.py
@@ -1,9 +1,9 @@
 from typing import Any
 
+import cads_adaptors
 import cads_adaptors.constraints
 import cads_catalogue
 import fastapi
-from cads_adaptors import AbstractAdaptor
 
 from . import adaptors, db_utils, exceptions, utils
 
@@ -16,7 +16,7 @@ def apply_constraints(
     catalogue_sessionmaker = db_utils.get_catalogue_sessionmaker()
     with catalogue_sessionmaker() as catalogue_session:
         dataset = utils.lookup_resource_by_id(process_id, record, catalogue_session)
-    adaptor: AbstractAdaptor = adaptors.instantiate_adaptor(dataset)
+    adaptor: cads_adaptors.AbstractAdaptor = adaptors.instantiate_adaptor(dataset)
     try:
         constraints = adaptor.apply_constraints(request=request)
     except cads_adaptors.constraints.ParameterError as exc:
