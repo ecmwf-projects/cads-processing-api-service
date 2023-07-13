e4709666c89a9202cd3fad7947dac45d33191ca3
Eddy Comyn-Platt <53045993+EddyCMWF@users.noreply.github.com>
2023-07-13 10:16:01 +0000

    remove dependancy on cads-adaptors directory structure (#128)
    * remove dependancy on cads-adaptors directory structure
    
    * qa
    
    * explicit import
    
    ---------
    
    Co-authored-by: Marco Cucchi <m.cucchi@bopen.eu>

--- cads_processing_api_service/constraints.py
+++ cads_processing_api_service/constraints.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-import cads_adaptors.adaptor
+import cads_adaptors
 import cads_adaptors.constraints
 import cads_catalogue
 import fastapi
@@ -16,9 +16,7 @@ def apply_constraints(
     catalogue_sessionmaker = db_utils.get_catalogue_sessionmaker()
     with catalogue_sessionmaker() as catalogue_session:
         dataset = utils.lookup_resource_by_id(process_id, record, catalogue_session)
-    adaptor: cads_adaptors.adaptor.AbstractAdaptor = adaptors.instantiate_adaptor(
-        dataset
-    )
+    adaptor: cads_adaptors.AbstractAdaptor = adaptors.instantiate_adaptor(dataset)
     try:
         constraints = adaptor.apply_constraints(request=request)
     except cads_adaptors.constraints.ParameterError as exc:
