c74faa1b26cc2e2cde9ad87ace1b06aa3f9d539b
Marco Cucchi <m.cucchi@bopen.eu>
2023-07-24 17:58:32 +0200

    refactor system request

--- cads_processing_api_service/adaptors.py
+++ cads_processing_api_service/adaptors.py
@@ -70,10 +70,10 @@ def make_system_job_kwargs(
     system_job_kwargs = {
         "entry_point": adaptor_properties["entry_point"],
         "setup_code": adaptor_properties["setup_code"],
+        "adaptor_config": adaptor_properties["config"],
         "resources": resources,
         "kwargs": {
             "form": adaptor_properties["form"],
-            "config": adaptor_properties["config"],
             "request": request["inputs"],
         },
     }
