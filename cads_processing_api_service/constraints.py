984d13244fc371e02b67bbc0e7e6befd0ee0b9bc
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2023-08-08 14:20:00 +0100

    qa

--- cads_processing_api_service/constraints.py
+++ cads_processing_api_service/constraints.py
@@ -1,9 +1,10 @@
 from typing import Any
 
-# import cads_adaptors
-from cads_adaptors.constraints import validate_constraints, ParameterError
 import cads_catalogue
 import fastapi
+
+# import cads_adaptors
+from cads_adaptors.constraints import ParameterError, validate_constraints
 
 from . import adaptors, db_utils, exceptions, utils
 
@@ -17,13 +18,13 @@ def apply_constraints(
     with catalogue_sessionmaker() as catalogue_session:
         dataset = utils.lookup_resource_by_id(process_id, record, catalogue_session)
 
-    
     adaptor_properties = adaptors.get_adaptor_properties(dataset)
     # Why are we catching this error here and not letting it fail where it fails?
     try:
         constraints = validate_constraints(
-            adaptor_properties.get("form", []), request,
-            adaptor_properties.get("config", {}).get("constraints", [])
+            adaptor_properties.get("form", []),
+            request,
+            adaptor_properties.get("config", {}).get("constraints", []),
         )
     except ParameterError as exc:
         raise exceptions.InvalidParameter(detail=str(exc))
