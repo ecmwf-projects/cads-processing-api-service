1e48296d81f3544ed158b0973e59797327c7cb3d
Marco Cucchi <m.cucchi@bopen.eu>
2023-10-05 10:09:33 +0200

    implement retry on write hostname for get job

--- cads_processing_api_service/clients.py
+++ cads_processing_api_service/clients.py
@@ -355,11 +355,22 @@ class DatabaseClient(ogc_api_processes_fastapi.clients.BaseClient):
         """
         user_uid = auth.authenticate_user(auth_header, portal_header)
         portals = [p.strip() for p in portal_header.split(",")]
-        compute_sessionmaker = db_utils.get_compute_sessionmaker(
-            mode=db_utils.ConnectionMode.read
-        )
-        with compute_sessionmaker() as compute_session:
-            job = utils.get_job_from_broker_db(job_id=job_id, session=compute_session)
+        try:
+            compute_sessionmaker = db_utils.get_compute_sessionmaker(
+                mode=db_utils.ConnectionMode.read
+            )
+            with compute_sessionmaker() as compute_session:
+                job = utils.get_job_from_broker_db(
+                    job_id=job_id, session=compute_session
+                )
+        except ogc_api_processes_fastapi.exceptions.NoSuchJob:
+            compute_sessionmaker = db_utils.get_compute_sessionmaker(
+                mode=db_utils.ConnectionMode.write
+            )
+            with compute_sessionmaker() as compute_session:
+                job = utils.get_job_from_broker_db(
+                    job_id=job_id, session=compute_session
+                )
         if job["portal"] not in portals:
             raise ogc_api_processes_fastapi.exceptions.NoSuchJob()
         auth.verify_permission(user_uid, job)
@@ -413,14 +424,31 @@ class DatabaseClient(ogc_api_processes_fastapi.clients.BaseClient):
         structlog.contextvars.bind_contextvars(job_id=job_id)
         user_uid = auth.authenticate_user(auth_header, portal_header)
         structlog.contextvars.bind_contextvars(user_id=user_uid)
-        compute_sessionmaker = db_utils.get_compute_sessionmaker(
-            mode=db_utils.ConnectionMode.read
-        )
-        with compute_sessionmaker() as compute_session:
-            job = utils.get_job_from_broker_db(job_id=job_id, session=compute_session)
-            auth.verify_permission(user_uid, job)
-            results = utils.get_results_from_broker_db(job=job, session=compute_session)
-            handle_download_metrics(job, results)
+        try:
+            compute_sessionmaker = db_utils.get_compute_sessionmaker(
+                mode=db_utils.ConnectionMode.read
+            )
+            with compute_sessionmaker() as compute_session:
+                job = utils.get_job_from_broker_db(
+                    job_id=job_id, session=compute_session
+                )
+                auth.verify_permission(user_uid, job)
+                results = utils.get_results_from_broker_db(
+                    job=job, session=compute_session
+                )
+        except ogc_api_processes_fastapi.exceptions.NoSuchJob:
+            compute_sessionmaker = db_utils.get_compute_sessionmaker(
+                mode=db_utils.ConnectionMode.write
+            )
+            with compute_sessionmaker() as compute_session:
+                job = utils.get_job_from_broker_db(
+                    job_id=job_id, session=compute_session
+                )
+                auth.verify_permission(user_uid, job)
+                results = utils.get_results_from_broker_db(
+                    job=job, session=compute_session
+                )
+        handle_download_metrics(job, results)
         return results
 
     def delete_job(
