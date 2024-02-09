3e2f6cf08fca24da39eb8763d94f7764f55899a4
Marco Cucchi <m.cucchi@bopen.eu>
2024-02-09 15:52:09 +0100

    introduce costing endpoint

---
+++ cads_processing_api_service/costing.py
@@ -0,0 +1,29 @@
+from typing import Any
+
+import cads_adaptors
+import cads_adaptors.constraints
+import cads_catalogue
+import fastapi
+
+from . import adaptors, db_utils, exceptions, utils
+
+
+def estimate_costing(
+    process_id: str = fastapi.Path(...),
+    request: dict[str, Any] = fastapi.Body(...),
+) -> dict[str, Any]:
+    table = cads_catalogue.database.Resource
+    catalogue_sessionmaker = db_utils.get_catalogue_sessionmaker(
+        db_utils.ConnectionMode.read
+    )
+    with catalogue_sessionmaker() as catalogue_session:
+        dataset = utils.lookup_resource_by_id(
+            resource_id=process_id, table=table, session=catalogue_session
+        )
+    adaptor: cads_adaptors.AbstractAdaptor = adaptors.instantiate_adaptor(dataset)
+    try:
+        costing: dict[str, Any] = adaptor.estimate_costing(request=request)
+    except cads_adaptors.constraints.ParameterError as exc:
+        raise exceptions.InvalidParameter(detail=str(exc))
+
+    return costing
