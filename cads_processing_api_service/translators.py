f3a9c973f72b43814530d10092291a0b695fc533
Marco Cucchi <m.cucchi@bopen.eu>
2024-07-09 15:59:06 +0200

    fix types

--- cads_processing_api_service/translators.py
+++ cads_processing_api_service/translators.py
@@ -143,19 +143,18 @@ def make_request_labels(
         "GeographicExtentWidget",
         "GeographicExtentMapWidget",
     ):
-        value_ids: list[str] = input_value_ids
         request_labels = [
             f"{label}: {value}°"
             for label, value in zip(
                 ["North", "West", "South", "East"],
-                value_ids,
+                input_value_ids,
             )
         ]
     elif cds_input_schema["type"] == "GeographicLocationWidget":
-        value_ids: dict[str, Any] = input_value_ids[0]
+        location = input_value_ids[0]
         try:
-            latitude = f"{value_ids['latitude']}°"
-            longitude = f"{value_ids['longitude']}°"
+            latitude = f"{location['latitude']}°"
+            longitude = f"{location['longitude']}°"
         except Exception as e:
             logger.error(
                 "Error extracting latitude and longitude from geographic location",
@@ -167,7 +166,6 @@ def make_request_labels(
             f"Longitude: {longitude}",
         ]
     else:
-        value_ids: list[str] = input_value_ids
         input_value_label = extract_labels(cds_input_schema)
         request_labels = []
         for input_value_id in input_value_ids:
