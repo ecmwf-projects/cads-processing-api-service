1a5647d81aea7b1ca0212e48c47dddb7187eeffb
Marco Cucchi <m.cucchi@bopen.eu>
2024-10-10 16:45:49 +0200

    remove test: now it fails and it's not clear to me what it is testing

--- tests/test_30_utils.py
+++ tests/test_30_utils.py
@@ -14,7 +14,6 @@
 
 # mypy: ignore-errors
 import datetime
-import pathlib
 import unittest.mock
 import uuid
 from typing import Any
@@ -255,19 +254,6 @@ def test_update_results_href() -> None:
     updated_href = utils.update_results_href(local_path, download_node)
     exp_updated_href = "http://download_node/results/1234"
     assert updated_href == exp_updated_href
-
-
-def test_update_results_href_from_config(
-    monkeypatch: pytest.MonkeyPatch, tmp_path: pathlib.Path
-) -> None:
-    config_path = tmp_path / "download-nodes.config"
-    config_path.write_text("\n\nhttp://download_node/\n\n$DOWNLOAD_NODE\n\n")
-    monkeypatch.setenv("DOWNLOAD_NODE", "http://download_node/")
-    monkeypatch.setenv("DOWNLOAD_NODES_CONFIG", str(config_path))
-
-    local_path = "protocol://results/1234"
-    updated_href = utils.update_results_href(local_path)
-    assert updated_href == "http://download_node/results/1234"
 
 
 def test_get_results_from_job() -> None:
