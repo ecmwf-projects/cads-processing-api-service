98bcb6fdb4e4bf5e20928d0154279c41a4b83ccd
Marco Cucchi <m.cucchi@bopen.eu>
2024-10-13 17:02:43 +0200

    refactor settings import

--- cads_processing_api_service/limits.py
+++ cads_processing_api_service/limits.py
@@ -1,7 +1,25 @@
+"""CADS Processing API rate limits."""
+
+# Copyright 2022, European Union.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
 import limits
 import structlog
 
 from . import config, exceptions, models
+
+SETTINGS = config.settings
 
 logger: structlog.stdlib.BoundLogger = structlog.get_logger(__name__)
 
@@ -10,9 +28,7 @@ limiter = limits.strategies.FixedWindowRateLimiter(storage)
 
 
 def check_rate_limit(method: str, auth_info: models.AuthInfo) -> None:
-    rate_limits_config: dict[str, list[str]] = config.ensure_settings().rate_limits.get(
-        method, {}
-    )
+    rate_limits_config: dict[str, list[str]] = SETTINGS.rate_limits.get(method, {})
     rate_limit_ids: list[str] = rate_limits_config.get(auth_info.request_origin, [])
     rate_limits = [limits.parse(rate_limit_id) for rate_limit_id in rate_limit_ids]
     rate_limits_exceeded = [
